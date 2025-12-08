783c0d1c33cb5dee183a809ce46ef12956d08068
jsparrowio <jsparrowio@outlook.com>
2025-12-07 17:39:48 -0800

    changed from connection check to basic welcome message

--- src/App.tsx
+++ src/App.tsx
@@ -13,11 +13,11 @@ function App() {
         <img src={fhsicon} style={{ 'height': '150px', 'width': '150px' }} alt="fierrettspherelogo"></img>
         <h1 className="title">The Fierrett Sphere</h1>
         <div className="message-container">
-          <IpPinger
-            ip="https://sso.fierrettsphere.com"
-            redirectUrl="https://sso.fierrettsphere.com/"
-            timeoutMs={2500}
-          />
+          <h1>Welcome to the Fierrett Sphere!</h1>
+          <p>Hey there! This is the landing page for the Fierrett Sphere, a private network provided by jsparrowio. No public access is allowed on this network.</p>
+          <p>If you have been granted access to TFS, make sure you enter the URL of the app you are attempting to reach, and login using the login credentials you created.</p>
+          <p>If you need further assistance, contact the administrator.</p>
+          <h2>Thank you!</h2>
         </div>
       </div>
     </div>
